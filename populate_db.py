a01916c30efe7ee3eb6e5f100c9fe3c377d07712
joshuahenriques <joshuahenriques95@gmail.com>
2021-07-12 15:40:36 -0400

    Reformat

--- populate_db.py
+++ populate_db.py
@@ -5,18 +5,21 @@ import requests
 import json
 import time
 
+
 def cus_cnt():
     raw_json = requests.get('http://172.105.3.51:8080/api/customers/list/customers')
     print(f'Customer count: {len(json.loads(raw_json.text))}')
 
+
 def task():
-    requests.post('http://172.105.3.51:8080/api/register/customer', json = customer.scrape())
+    requests.post('http://172.105.3.51:8080/api/register/customer', json=customer.main())
+
 
 def main():
     executor = ThreadPoolExecutor(max_workers=8)
-    
+
     i = 0
-    while(True):
+    while True:
         if i == 1000:
             i = 0
             cus_cnt()
